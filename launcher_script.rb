5dfd0aee4e3703f5d3708919f46e3f1c5338b6ca
Joseph <joseph@Froze.local>
2014-02-21 06:28:48 +0800

    Fix intent integration

--- launcher_script.rb
+++ launcher_script.rb
@@ -43,7 +43,7 @@ class Main < Activity
       end
       
       V('#qrcode').on(:click) do |view|
-        integrator = Java::com.droiuby.client.utils.intents.IntentIntegrator.new(me)
+        integrator = Java::com.droiuby.client.core.utils.intents.IntentIntegrator.new(me)
         integrator.initiateScan
       end
             
@@ -58,7 +58,7 @@ class Main < Activity
   
   def on_activity_result(request_code, result_code, intent)
     app_url = V('#app_url')
-    scanResult = Java::com.droiuby.client.utils.intents.IntentIntegrator.parseActivityResult(request_code, result_code, intent.native)
+    scanResult = Java::com.droiuby.client.core.utils.intents.IntentIntegrator.parseActivityResult(request_code, result_code, intent.native)
     if scanResult != nil
       app_url.text= scanResult.getContents()
     end
